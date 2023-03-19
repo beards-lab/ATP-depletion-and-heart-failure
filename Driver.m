2274c1228700c0960b394d0cd010550886448662
Filip Ježek <jezekf@gmail.com>
2023-03-19 13:10:21 +0100

    Experimenting with the kstiff

--- Driver.m
+++ Driver.m
@@ -15,38 +15,39 @@ ghostSave = '';
 % ghostSave = 'beardsOrig_passive';
 % ghostSave = 'ShiftingStrain40_Slim0_3';
 % ghostSave = 'ShiftingStrain80_Slim0_3';
-ghostSave = 'operatorSplittingPU020';
-ghostSave = 'operatorSplittingPU0';
-ghostSave = 'operatorSplittingPU080';
-ghostSave = 'ShiftingStrain160_Slim0_3';
-ghostSave = 'beardsOrig_all60';
-ghostSave = 'beardsOrig_OV_Pas_SS';
-
+% ghostSave = 'operatorSplittingPU020';
+% ghostSave = 'operatorSplittingPU0';
+% ghostSave = 'operatorSplittingPU080';
+% ghostSave = 'ShiftingStrain160_Slim0_3';
+% ghostSave = 'beardsOrig_all60';
+% ghostSave = 'beardsOrig_OV_Pas_SS';
+% ghostSave = 'beardsOrig';
+% ghostSave = 'bO_Fp31';
 % ghostSave = 'beardsOrig50'; % strain shifting dS 50 nm
 % ghostSave = 'beardsOrig5'; % strain shifting dS 5 nm
 % ghostSave = 'beardsOrig5_BW';%bells and whistles :)
 
 ghostLoad = '';
-% ghostLoad = 'beardsOrig';
+ghostLoad = 'beardsOrig';
 % ghostLoad = 'ShiftingStrain40';
 % ghostLoad = 'operatorSplitting'
-ghostLoad = 'operatorSplittingPU020';
-ghostLoad = 'operatorSplittingPU0';% N = 40
-ghostLoad = 'operatorSplittingPU080';
-ghostLoad = 'ShiftingStrain40_Slim0_3';
-ghostLoad = 'ShiftingStrain80_Slim0_3';
-ghostLoad = 'ShiftingStrain160_Slim0_3';
-ghostLoad = 'ShiftingStrainTest20';
-ghostLoad = 'ShiftingStrainTest40';
+% ghostLoad = 'operatorSplittingPU020';
+% ghostLoad = 'operatorSplittingPU0';% N = 40
+% ghostLoad = 'operatorSplittingPU080';
+% ghostLoad = 'ShiftingStrain40_Slim0_3';
+% ghostLoad = 'ShiftingStrain80_Slim0_3';
+% ghostLoad = 'ShiftingStrain160_Slim0_3';
+% ghostLoad = 'ShiftingStrainTest20';
+% ghostLoad = 'ShiftingStrainTest40';
 % ghostLoad = 'ShiftingStrainTest80';
 % ghostLoad = 'ShiftingStrainTest160';
 % ghostLoad = 'ShiftingStrainTest240';
 % ghostLoad = 'beardsOrig_all30';
-ghostLoad = 'beardsOrig_all60';
-ghostLoad = 'operatorSplittingPU0';% N = 40
-ghostLoad = 'beardsOrig5';% N = 40
-ghostLoad = 'beardsOrig5_BW';% N = 40
-% ghostLoad = '';
+% ghostLoad = 'beardsOrig_all60';
+% ghostLoad = 'operatorSplittingPU0';% N = 40
+% ghostLoad = 'beardsOrig5';% N = 40
+% ghostLoad = 'beardsOrig5_BW';% N = 40
+ghostLoad = 'bO_Fp31';
 
 % testing setup
 % params.UseOverlap = true;
@@ -54,10 +55,10 @@ ghostLoad = 'beardsOrig5_BW';% N = 40
 
 params.UseTORNegShift = false;
 params.UseMutualPairingAttachment = false;
-params.UseSlack = true;
-params.UseOverlap = true;
-params.UsePassive = true;
-params.UseSerialStiffness = true;
+params.UseSlack = false;
+params.UseOverlap = false;
+params.UsePassive = false;
+params.UseSerialStiffness = false;
 
 % params.alpha1 = 0;
 % params.alpha2 = 0;
@@ -65,17 +66,25 @@ params.UseSerialStiffness = true;
 % need a ksttiff1 and kstiff2 parameter retune
 params.F_act_UseP31 = true;
 params.UseP31Shift = true;
-
+% params.kstiff1 = -100000;
+% params.kstiff2 = 14000;
+params.kstiff1 = 10000;
+params.kstiff2 = 13000;
+% params.dr = -0.01;
 
 params.PlotEachSeparately = true;
+params.PlotFullscreen = false;
 params.ghostLoad = ghostLoad;
 params.ghostSave = ghostSave;
+params.UseSpeedHalving = true;
+
+%% init
+clf;
 
 % save as default
 params0 = getParams(params);
 params0.N
 
-%% init
 LoadData;
 t_ss = [0 1];
 t_sl0 = [0 0.1];
@@ -83,13 +92,17 @@ tic
 
 RunBakersExp;
 toc
+xlim('auto')
+ylim('auto')
 
 %% OPTIM
-% return
+return
 params0.mods = {"kstiff1", "kstiff2"};
 
+params0.PlotEachSeparately = false;
 options = optimset('Display','iter', 'TolFun', 1e-3, 'Algorithm','sqp', 'TolX', 0.1, 'PlotFcns', @optimplotfval, 'MaxIter', 1500);
 g = [1, 1];
+g = [1.2539    0.4422];
 optimfun = @(g)evaluateBakersExp(g, params0);
 x = fminsearch(optimfun, g, options)
 g = x;
