686ac0d0023eb18078afde78fa3508ac2c161b34
Jezek <jezekf@gmail.com>
2023-07-14 14:47:18 +0200

    Adding Dan;s updated codes

--- Dan/dadt.m
+++ Dan/dadt.m
@@ -5,5 +5,7 @@ s  = 0:ds:(L-ds);
 u = 1 - ds*sum(a);
 f = zeros(N,1);
 f(1) = + (r_a/ds)*u ; % attach rate
-% f = f - (r_d'.*(a.^2)); % de-attach rate
-f = f - (r_d'.*a.*(s'/0.4)); % de-attach rate
+f = f - r_d.*(a.^2).*(s'/0.4).^4; % de-attach rate is proportional to s^4
+% f = f - r_d.*(a.^2).*(exp(s'/0.4)-1); % de-attach rate is proportional to exp(s)
+% f = f - r_d.*(a.^2).*(exp(-s/4)'); % de-attach rate is proportinal to exp(-s)
+
--- dattdt.m
+++ dattdt.m
@@ -1,4 +1,4 @@
-function datt = dattdt(t,att, s_ap, r_a, r_d, F2vals)
+function datt = dattdt(t,att, s_ap, r_a, r_d, tearingForce)
 % passive attachment as a function of space
 
 u = 1 - sum(att);
@@ -6,5 +6,5 @@ datt = zeros(length(att), 1);
 datt(s_ap) = + (r_a)*u; % attach rate at the current center
 % datt = datt - r_d.*att; % de-attach rate
 
-datt = datt - max(att, 0).*(r_d + abs(F2vals)); % de-attach rate depends on force
+datt = datt - max(att, 0).*(r_d + 0.1*exp(-abs(tearingForce)/10)); % de-attach rate depends on force
 
