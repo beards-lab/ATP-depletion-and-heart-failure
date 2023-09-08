7a1ba3fc1f78e121d37a5762d67f06a46f1d349d
Jezek <jezekf@gmail.com>
2023-09-08 14:28:50 +0200

    POstoptim running SA

--- Dan/OptimizeCOmbined.m
+++ Dan/OptimizeCOmbined.m
@@ -51,17 +51,17 @@ mod(6) = 1.03;
 % mod(8) = 1.2;
 mod(13) = -2.8;
 %%
-mod = [2.0398    1.3113    3.8942    1.3500    0.4784 0.7398    0.8176    0.7869    0.8629    0.7200 1.3634    1   -2.8000    1.0150    0.6382 -0.5199];
+% mod = [2.0398    1.3113    3.8942    1.3500    0.4784 0.7398    0.8176    0.7869    0.8629    0.7200 1.3634    1   -2.8000    1.0150    0.6382 -0.5199];
 drawPlots = true;
 cost_sap = []; % SA plus
 cost_sam = []; % SA minus
-pCa = 4;
+pCa = 11;
 figure(100);
 RunCombinedModel;
 cost
-c0_4 = cost;
-%%
-saSet = [1:16];
+c0 = cost;
+%
+saSet = [1:9 12:15];
 % saSet = [1:8 13];
 % saSet = [14 15];
 for i_m = saSet
@@ -82,33 +82,35 @@ end
 % %%
 % cost11_sap = cost_sap;
 % cost11_sam = cost_sam;
+% c0_11 = c0;
 % %%
 % cost4_sap = cost_sap;
 % cost4_sam = cost_sam;
+% c0_4 = c0;
 
 % identify tradeoffs
 eps = (c0_4 + c0_11)*1e-3;
 % better result
 bett = (cost11_sap*10 + cost4_sap + eps < c0_11*10 + c0_4) ...
     | (cost11_sam*10 + cost4_sam + eps < c0_11*10 + c0_4);
-betts = strings(1, 16);betts(bett) = "+++";
+betts = strings(1, length(cost4_sap));betts(bett) = "+++";
 % tradeoffs
 tdo = (cost11_sap + eps < c0_11 & cost4_sap > c0_4 + eps) | (cost11_sam + eps < c0_11 & cost4_sam > c0_4 + eps) ...
     | (cost11_sap > c0_11 + eps & cost4_sap + eps < c0_4) | (cost11_sam > c0_11 + eps & cost4_sam + eps < c0_4);
-tdos = strings(1, 16);
-tdos(tdo) = "*";
+tdos = strings(1, length(cost4_sap));tdos(tdo) = "*";
 figure(101);clf; 
 b = bar([cost11_sap*10; cost4_sap; zeros(size(cost4_sap)); cost11_sam*10;cost4_sam]'); hold on;
 title('Sensitivity Anal')
 plot([0, 16], [c0_11 c0_11]*10, 'b--')
 plot([0, 16], [c0_4 c0_4], 'm--')
-title('Grouped sensitivity analysis')
+title('Grouped sensitivity analysis (* indicates trade-off, +++ potential of improvement)')
 legend('10xpCa11+','pCa4+','', '10xpCa11-','pCa4-','pCa11_0','pCa4_0')
-set(gca,'XTick',[1:16]);
-set(gca,'XTickLabels',strcat(string(1:16), ':', modNames,tdos, betts));
+set(gca,'XTick',saSet);
+set(gca,'XTickLabels',strcat(string(1:length(cost4_sap)), ':', modNames(1:15),tdos, betts   ));
+set(gca, "FontSize", 14)
 %%
 tic
-modSel = [2 3 4 6 7 8 9 14 15];
+modSel = [2 3 4 6 7 8 9 12 14 15];
 % mod(16) = 0;
 evalCombined(mod(modSel))
 toc
@@ -119,6 +121,8 @@ options = optimset('Display','iter', 'TolFun', 1e-3, 'Algorithm','sqp', 'TolX', 
 % x0 = [0.5795    1.6029    0.9047    1.0569    0.9236    0.6627    1.0497    0.9465    1.0641    0.7124];
 
 % best in the worst so far optMods =     1.3705    1.4770    1.1905    1.1205    0.8670    0.3074    0.6812    0.9407    1.3393    0.8915    1.1628    1.0166
+modSel = [2 3 4 6 7 8 9 12 14 15];
+
 init = mod(modSel);
 x = fminsearch(@evalCombined, init, options);
 
@@ -127,7 +131,7 @@ mod(modSel) = x;
 save mod;
 %% result
 mod = [2.0398    0.9359    4.3424    2.3068    0.4784 0.6410    0.8054    0.8220    0.2923    0.7200 1.3634    1.0000   -2.8000    1.3022    0.8551];
-
+mod = [2.0398    0.9359    4.3397    2.2756    0.4784 0.6412    0.8053    0.8333    0.2923    0.7200 1.3634    1.0468   -2.8000    1.3200    0.8759];
 %%
 function totalCost = evalCombined(optMods)
     %normal - optimizing for all
@@ -138,13 +142,13 @@ function totalCost = evalCombined(optMods)
     % mod([11, 12, 13]) = optMods(:);
     % x0 = [1.0504    1.2978    0.9544    1.0226    0.4784    1.0429    0.9584    0.8259    0.8629    0.7200    1.3634    0.8411    0.9660    1.0150 1 1];    
     % mod = [2.2000    4.4000    2.2000    1.0226    0.4784    1.0300    0.9584    0.8259    0.8629    0.7200    1.3634    0.8411   -2.8000    1.0150    1.0000    1.0000];
-    mod = [2.0398    1.3113    3.8942    1.3500    0.4784 0.7398    0.8176    0.7869    0.8629    0.7200 1.3634    1   -2.8000    1.0150    0.6382];
+    mod = [2.0398    0.9359    4.3424    2.3068    0.4784 0.6410    0.8054    0.8220    0.2923    0.7200 1.3634    1.0000   -2.8000    1.3022    0.8551];
 
-    modSel = [2 3 4 6 7 8 9 14 15];
+    modSel = [2 3 4 6 7 8 9 12 14 15];
     % mod([1:4 6:10 13]) = optMods;
     mod(modSel) = optMods;
 
-    drawPlots = true;
+    drawPlots = false;
     %% no Ca
     pCa = 11;
     if drawPlots
